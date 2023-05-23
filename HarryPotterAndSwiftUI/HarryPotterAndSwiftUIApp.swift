f0db9830d2d24c5ffb30659aa7dfe789f77e9373
Pavel Grigorev <amorfien@yandex.ru>
2023-05-23 19:35:50 +0300

    Initial Commit

---
+++ HarryPotterAndSwiftUI/HarryPotterAndSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HarryPotterAndSwiftUIApp.swift
+//  HarryPotterAndSwiftUI
+//
+//  Created by Pavel Grigorev on 23.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct HarryPotterAndSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
